6df2de5a938af25f41bc20e96e146b86a30f2366
Misbah Anjum N <misanjumn@ibm.com>
2025-11-07 14:41:43 +0530

    Add virtual-pilot.py script to run per suite/test
    Command: python3 virtual-pilot.py --config config/suites/<suite>.yaml
    This script is used to run tool automation scripts in per test basis
    
    Signed-off-by: Misbah Anjum N <misanjumn@ibm.com>

---
+++ virtual-pilot.py
@@ -0,0 +1,20 @@
+# python3 main.py --config config/custom_suite.yaml
+
+import argparse
+from orchestrator import run_suite_from_config
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config",
+        default="config/avocado_suite.yaml",
+        help="Path to YAML file listing which tests to run"
+    )
+    args = parser.parse_args()
+
+    result, error = run_suite_from_config(args.config)
+    if not result:
+        print(f"\nTest failed: {args.config}\nFailure: {error}")
+
+if __name__ == "__main__":
+    main()
